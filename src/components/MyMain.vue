b6e05a0e59fe0107784395b4d511b3d808ac0cd4
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-03-06 11:34:21 +0100

    main finito

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -294,6 +294,96 @@
                 </div>
             </div>
         </section>
+        <section class="ms-sezione-newsletter d-flex align-items-center">
+            <div class="container">
+                <div class="row ms-style text-light justify-content-center">
+                    <div class="col-5">
+                        <h6 class="mb-4">NEWSLETTER</h6>
+                         <span class="ms-testo-background rounded-3 me-2">Know</span> <span>First</span>
+                        <div class="d-flex align-items-center">
+                                <h5 class="my-4">
+                                    Follow closely and receive content about our company 
+                                    and the news of the current marchet    
+                                </h5>
+                        </div>
+                    </div>
+                    <div class="col-5">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control style-input" id="floatingInput" placeholder="Name">
+                            <label for="floatingInput">Name</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="email" class="form-control style-input mb-3" id="floatingPassword" placeholder="Email">
+                            <label for="floatingPassword">Email</label>
+                        </div>
+                        <button class="ms-button">SUBSCRIBE</button>
+                    </div>
+                </div>
+            </div>   
+        </section>
+        <section class="ms-sezione-form ms-padding">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-8 ms-style pe-5">
+                        <h6 class="mb-4">SEND A MESSAGE</h6>
+                        <span>Get in</span> <span class="ms-testo-background testo-2 rounded-3 ms-2">Touch</span>
+                                <h5 class="my-4">
+                                    We will respond to your message as soon as possible    
+                                </h5>                        
+                        <form class="row g-3">
+                            <div class="col-6">
+                                <input type="text" class="form-control ms-input" placeholder="Name">
+                            </div>
+
+                            <div class="col-6">
+                                <input type="email" class="form-control ms-input" placeholder="Email">
+                            </div>
+                                              
+                            <div class="col-6">
+                                <input type="tel" class="form-control ms-input" placeholder="Phone" id="phone">
+                            </div>
+
+                            <div class="col-6">
+                                <select class="form-select ms-input">
+                                    <option selected>More info</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+
+                            <div class="col-12">
+                                <textarea class="form-control ms-input" id="exampleFormControlTextarea1" rows="5" placeholder="Message"></textarea>
+                            </div>
+
+                            <div>
+                               <button class="ms-button">SEND</button> 
+                            </div>
+                            
+                        </form>
+                    </div>
+                    <div class="col-3">
+                        <h4 class="mb-4">Example Inc.</h4>
+                        <h6 class="mb-4 form-h6">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h6>
+                        <h6 class="mb-4 form-h6">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h6>
+                        <div class="mb-4 d-flex align-items-center">
+                            <i class="fa-solid fa-phone icon-form me-2"></i>
+                            <span class="form-span">+1 (305) 1234-5678</span>
+                        </div>
+                        <div class="mb-4 d-flex align-items-center">
+                            <i class="fa-solid fa-envelope icon-form me-2"></i>
+                            <span class="form-span">hello@example.com</span>
+                        </div>
+                        <div class="mb-4 d-flex align-items-center">
+                            <i class="fa-solid fa-location-dot icon-form me-2"></i>
+                            <span class="form-span">Main Avenue, 987</span>
+                        </div>
+                        
+                        <button class="ms-button-form">VIEW MAP</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
     
 </template>
@@ -383,11 +473,11 @@ export default {
         }
 
         .icon-team{
-            padding: 10px 12px;
+            padding: 10px 10px;
             background-color: #daeced;
             color: #058283;
             border-radius: 50%;
-            margin: 0 5px;
+
         }
 
         .box-paragrafo{
@@ -421,5 +511,52 @@ export default {
             background-color: rgba(0, 0, 0, 0.5);
             top: 0;
         }
+    }
+
+    .ms-sezione-newsletter{
+        width: 100%;
+        height: 450px;
+        background-color: #111117;
+
+        .style-input{
+            background-color: #1d1d23 !important;
+            color: white !important;
+            border: none;
+        }
+    }
+
+    .ms-sezione-form{
+        width: 100;
+        height: 700px;
+
+        .ms-input{
+            background-color: #ececec !important;
+            border: none;
+        }
+
+        .ms-button-form{
+            color: #058283;
+            background-color: rgba(0, 0, 0, 0);
+            border: solid 1px #058283;
+            padding: 10px 20px;
+            border-radius: 5px;
+        }
+
+        .icon-form{
+            padding: 10px 12px;
+            background-color: #daeced;
+            color: #058283;
+            border-radius: 50%;
+            margin: 0 5px;
+        }
+
+        .form-span{
+            color: #058283;
+        }
+
+        .form-h6{
+            color: #a2a9af;
+        }
+        
     }
 </style>
